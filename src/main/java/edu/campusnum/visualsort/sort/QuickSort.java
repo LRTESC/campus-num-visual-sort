0fbab84e892a3f9613f433cf847362f121e95285
lrtesc <escurelaurent@gmail.com>
2023-08-30 15:13:26 +0200

    QuickSort done

--- src/main/java/edu/campusnum/visualsort/sort/QuickSort.java
+++ src/main/java/edu/campusnum/visualsort/sort/QuickSort.java
@@ -9,39 +9,32 @@ import edu.campusnum.visualsort.model.ObservableArray;
  * Time: 16:31
  */
 public class QuickSort implements SortAlgorithm {
-    int premier;
-    int dernier;
-    int partition;
-    int quickSort;
 
     @Override
 
     public void sort(ObservableArray array) {
-        if (2 <= array.getLength()) {
+        quickSort(array,0,array.getLength()-1);
+    }
+
+    private void quickSort(ObservableArray array, int premier, int dernier) {
+        if (1 < (dernier - premier + 1 )) {
             int pi = partition(array, premier, dernier);
             quickSort(array, premier, pi - 1);
             quickSort(array, pi + 1, dernier);
         }
     }
 
-    private void quickSort(ObservableArray array, int premier, int i) {
 
-    }
-
-    private int partition(ObservableArray array, int premier, int dernier) {
-        return premier;
-    }
-
-    public void setPartition(ObservableArray array, int premier, int dernier) {
+    public int partition(ObservableArray array, int premier, int dernier) {
         int pivot = dernier;
         int j = premier;
         for (int i = premier; i < dernier; i++) {
             if (array.get(i) <= array.get(pivot)) {
-                array.swap((i), (j));
+                array.swap(i, j);
                 j = j + 1;
             }
         }
-        array.swap((dernier), j);
+        array.swap(dernier, j);
         return j;
     }
 }
